aa6ce0ae14ccf1842fd0e3b021febbd00a2df489
Leon Drexhage <drexhage@users.noreply.github.com>
2023-11-12 13:02:06 +0100

    feat: Persist store in localstorage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,22 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import tableReducer from '../features/table/tableSlice';
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedTableReducer = persistReducer(persistConfig, tableReducer)
 
 export const store = configureStore({
   reducer: {
-    table: tableReducer,
+    table: persistedTableReducer,
   },
 });
+
+export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
