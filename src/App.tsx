c9f014a54200a18206b76dbaa2d78283033cd0ce
Leon Drexhage <drexhage@users.noreply.github.com>
2023-12-01 22:23:00 +0100

    feat: add mui and data grid

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import {
   unloadTable,
 } from "./features/table/tableSlice";
 import { buildPyScript, testScript } from "./PyScriptUtils";
+import Button from '@mui/material/Button';
+import { DataGrid } from '@mui/x-data-grid';
 
 function App() {
   let dispatch = useAppDispatch();
@@ -27,11 +29,16 @@ function App() {
     let files = e.target?.files!;
     reader.readAsText(files[0]);
   };
+  let columns = table.headers.map(x => ({ field: x, editable: true }))
+  let i = 0;
+  let rows = table.data.map(x => ({id: i++, ...x}))
 
   if (table.loaded) {
     return (
       <div>
-        <button onClick={() => dispatch(unloadTable())}>Unload</button>
+        <Button variant="contained" onClick={() => dispatch(unloadTable())}>Unload</Button>
+        <DataGrid rows={rows} columns={columns} />
+
         <hr />
         {JSON.stringify(table.data, null, 2)}
       </div>
@@ -40,18 +47,19 @@ function App() {
     return (
       <div className="App">
         <label htmlFor="file">
-          <button
+          <Button
+            variant="contained"
             style={{ cursor: "pointer" }}
             onClick={() => inputFile.current?.click()}
           >
             Import CSV
-          </button>
-          <button style={{ cursor: "pointer" }} onClick={execute}>
+          </Button>
+          <Button variant="outlined" style={{ cursor: "pointer" }} onClick={execute}>
             Load script
-          </button>
-          <button style={{ cursor: "pointer" }} onClick={remove}>
+          </Button>
+          <Button variant="outlined" style={{ cursor: "pointer" }} onClick={remove}>
             Remove script
-          </button>
+          </Button>
         </label>
         <input
           type="file"
